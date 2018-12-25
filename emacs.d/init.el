44c62ef16593c7b5b675a96c3ae25bb8132ab7c1
Stefan Magnuson <styrmis@gmail.com>
2018-12-25 22:54:58 +0000

    Replace recursive addition of org agenda files with simpler version
    The simpler version does not recursively-traverse the directory
    structure.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -106,33 +106,8 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
 
-;; recursively find .org files in provided directory
-;; modified from an Emacs Lisp Intro example
-(defun sa-find-org-file-recursively (&optional directory filext)
-  "Return .org and .org_archive files recursively from DIRECTORY.
-If FILEXT is provided, return files with extension FILEXT instead."
-  (interactive "Directory: ")
-  (let* (org-file-list
-	 (case-fold-search t)	      ; filesystems are case sensitive
-	 (file-name-regex "^[^.#].*") ; exclude dot, autosave, and backup files
-	 (filext (or filext "org$\\\|org_archive"))
-	 (fileregex (format "%s\\.\\(%s$\\)" file-name-regex filext))
-	 (cur-dir-list (directory-files directory t file-name-regex)))
-    ;; loop over directory listing
-    (dolist (file-or-dir cur-dir-list org-file-list) ; returns org-file-list
-      (cond
-       ((file-regular-p file-or-dir) ; regular files
-	(if (string-match fileregex file-or-dir) ; org files
-	    (add-to-list 'org-file-list file-or-dir)))
-       ((file-directory-p file-or-dir)
-	(dolist (org-file (sa-find-org-file-recursively file-or-dir filext)
-			  org-file-list) ; add files found to result
-	  (add-to-list 'org-file-list org-file)))))))
-
-;; Recursively load all org files into agenda
-;; From http://blog.hourglasslab.com/2016/10/19/how%20do%20i%20organize%20my%20tasks%20and%20notes/
-(setq org-agenda-files
-      (append (sa-find-org-file-recursively "~/Dropbox/org/" "org")))
+(customize-set-variable 'org-directory "~/org")
+(customize-set-variable 'org-agenda-files (list org-directory))
 
 (setq org-modules '(org-habit))
 
