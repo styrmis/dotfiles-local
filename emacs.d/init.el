0716f3c95d299dfb36061b7d0313afb6398b09d6
Stefan Magnuson <styrmis@gmail.com>
2018-12-26 22:47:30 +0000

    Integrate evil-collection

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -26,8 +26,17 @@
 ;; Vim mode
 (use-package evil
   :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
 
 (use-package evil-escape
   :ensure t
