b125b99300f962ce3413bf1727c7cdc003044faf
Stefan Magnuson <styrmis@gmail.com>
2020-02-16 21:55:59 +0000

    Add install code for straight Emacs package manager

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,18 @@
 ;; Following https://huytd.github.io/emacs-from-scratch.html
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;; Package configs
 (require 'package)
 (setq package-enable-at-startup nil)
