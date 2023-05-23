6f9d19055bc46cecedc9e1ac50557bc3c3742f8f
Stefan Magnuson <styrmis@gmail.com>
2023-05-23 11:36:33 +0100

    Define firenvim_config object in full

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -77,7 +77,13 @@ return {
       vim.fn["firenvim#install"](0)
     end,
     config = function()
-      vim.g.firenvim_config.localSettings['.*'] = { selector = 'textarea', takeover = 'never' }
+      vim.g.firenvim_config = {
+        localSettings = {
+          [ [[.*]] ] = {
+            takeover = 'never'
+          }
+        }
+      }
     end
   }
 }
