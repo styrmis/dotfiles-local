6714fd6813541aebe730fce2117f4269a55b0290
Stefan Magnuson <styrmis@gmail.com>
2023-05-23 09:55:24 +0100

    Integrate firenvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -66,4 +66,18 @@ return {
   "nvim-lua/popup.nvim",
   "udalov/kotlin-vim", -- Has treesitter support but prefer the plugin
   "dense-analysis/ale",
+  {
+    'glacambre/firenvim',
+
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    cond = not not vim.g.started_by_firenvim,
+    build = function()
+      require("lazy").load({ plugins = "firenvim", wait = true })
+      vim.fn["firenvim#install"](0)
+    end,
+    config = function()
+      vim.g.firenvim_config.localSettings['.*'] = { selector = 'textarea', takeover = 'never' }
+    end
+  }
 }
