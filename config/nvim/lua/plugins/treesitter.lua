cd270cd02dd0602bafce215848f12a05260f8344
Stefan Magnuson <styrmis@gmail.com>
2024-08-15 17:49:18 +0900

    Replace vim-based invocation of TSUpdate with Lua

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-    vim.cmd([[:TSUpdate]])
-  end,
+  build = ":TSUpdate",
   config = function()
     require'nvim-treesitter.configs'.setup {
       highlight = {
