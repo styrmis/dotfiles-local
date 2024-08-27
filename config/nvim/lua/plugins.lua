bd1b6b53239bcc70030950d7612b1ed1c942fc0c
Stefan Magnuson <stefan.magnuson@shopify.com>
2024-08-27 12:04:29 +0100

    Disable lazy loading for firenvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -69,11 +69,8 @@ return {
   {
     'glacambre/firenvim',
 
-    -- Lazy load firenvim
-    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-    cond = not not vim.g.started_by_firenvim,
+    -- Not lazy loading, as it stopped working
     build = function()
-      require("lazy").load({ plugins = "firenvim", wait = true })
       vim.fn["firenvim#install"](0)
     end,
     config = function()
