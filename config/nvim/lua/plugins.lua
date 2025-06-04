b390e14c0d385d3f90ac279ac1e7b6e2014f3ec3
Stefan Magnuson <stefan.magnuson@shopify.com>
2025-06-04 12:01:19 +0100

    Add claudecode.nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -196,4 +196,12 @@ return {
       }
     }
   },
+  {
+    "coder/claudecode.nvim",
+    config = true,
+    keys = {
+      { "<leader>cc", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+      { "<leader>cs", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+    },
+  }
 }
